d103f4f8c7f25ce24191810a12f896aff7a0ba78
Lena Mangold <lena@mangold.org>
2021-08-12 08:28:47 -0500

    make background height important

--- R/style.R
+++ R/style.R
@@ -103,7 +103,7 @@ create_element_styles <- function(opts_theme = NULL){
                             "padding-left:10px;",
                             "padding-right:10px;",
                             "padding-bottom: ",padding_bottom,"px;",
-                            "height: 100%;")
+                            "height: 100% !important;")
 
 
   logoStyle <- get_logo_style(opts_theme = opts_theme)
